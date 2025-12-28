680679bea1b22890a412ed97612811402867a208
kumaruseru <nghiaht281003@gmail.com>
2025-12-28 15:17:44 +0700

    fix(backend): switch to smtp port 465 ssl for better reliability

--- backend/settings.py
+++ backend/settings.py
@@ -139,8 +139,9 @@ else:
 # --- EMAIL ---
 EMAIL_BACKEND = 'apps.utils.email_backend.ForceIPv4EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = env.int('EMAIL_PORT', default=587)
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_PORT = env.int('EMAIL_PORT', default=465)
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
